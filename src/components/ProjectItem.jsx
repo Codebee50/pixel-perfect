81577b9c95696695006d79c8819f4f303e829194
Code bee <codebee345@outlook.com>
2024-05-26 17:13:57 +0100

    mods

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -9,13 +9,13 @@ const ProjectItem = (props) => {
         </div>
 
         <div className="absolute w-full top-0 h-full bg-gradient-to-t from-black z-10 rounded-lg flex flex-col p-5 justify-end">
-            <p className="text-white font-medium text-[1.5rem] sm:text-[1.7rem] leading-9 font-steradian">{props.title}</p>
+            <p className="text-white font-medium text-[1.4rem] sm:text-[1.7rem] leading-9 font-steradian">{props.title}</p>
             <p className="font-steradian text-gray100 font-light">{props.name}</p>
 
             <div className="w-full flex flex-row items-center gap-4 mt-5 flex-wrap">
                 {
                     props.highlights.map((highlight)=>(
-                        <div key={highlight} className="bg-white px-5 py-1 rounded-full"><p className="font-steradian text-sm">{highlight}</p></div>
+                        <div key={highlight} className="bg-white px-5 py-1 rounded-full"><p className="font-steradian text-[0.75rem] sm:text-sm">{highlight}</p></div>
                     ))
                 }
             </div>
