34fae78c3485ad8855f8dba99c26e6bbd58e577f
Code bee <codebee345@outlook.com>
2024-05-26 17:10:41 +0100

    modified width of bars

--- src/components/LongBar.jsx
+++ src/components/LongBar.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 
 const LongBar = (props) => {
   return (
-    <motion.div layout className={`w-10 min-[444px]:w-14 bg-gradient-to-t from-purple100 to-green100 ${props.className}`}
+    <motion.div layout className={`w-7 min-[337px]:w-10 min-[444px]:w-14 bg-gradient-to-t from-purple100 to-green100 ${props.className}`}
     initial={{opacity:0, height:0}}
     animate={{opacity:1, height:`auto`}}
     transition={{ duration: 0.9, delay:props.index*0.3, type:"tween"}}>
