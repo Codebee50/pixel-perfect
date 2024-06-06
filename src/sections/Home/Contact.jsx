8ca5d28e9e04c02cd0d9efdb2fcfe6c45a0c4191
Code bee <codebee345@outlook.com>
2024-06-06 08:14:58 +0100

    made corners rounded

--- src/sections/Home/Contact.jsx
+++ src/sections/Home/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section className="w-full min-h-screen hero-padding pt-5">
         
-        <div className="w-full h-[200px] bg-slate-300 relative">
+        <div className="w-full h-[200px] bg-slate-300 relative rounded-lg overflow-hidden">
             <img src={workTogether} className="w-full h-full object-cover object-center" alt="" />
             <div className="w-full h-full absolute bg-bg-overlay top-0 p-6 flex items-end">
                 <p className="text-white font-bold font-steradian text-4xl">Work With Us</p>
