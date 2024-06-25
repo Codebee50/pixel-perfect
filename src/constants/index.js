ec80e5979d18c1b9a49573ba977bd2e4906bbb58
Code bee <codebee345@outlook.com>
2024-06-25 19:47:52 +0100

    mods

--- src/constants/index.js
+++ src/constants/index.js
@@ -130,8 +130,8 @@ export const footerLinks = [
   {
       title: "Get in touch",
       links: [
-          { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-          { name: "+92554862354", link: "tel:+92554862354" },
+          { name: "hello@pixelperfect.com", link: "mailto:hello@pixelperfect.com" },
+          { name: "+92554862354", link: "tel:+08143015512" },
       ],
   },
 ];
